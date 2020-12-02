3eae578cd85f72cac846cea481d2e487d2553ac4
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-02 17:36:01 +0900

    typeorm 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
@@ -7,6 +8,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'hong',
+      password: '12345',
+      database: 'uber-eats',
+      synchronize: true, // 데이터베이스 모든 모듈 동기화 여부
+      logging: true,
     }),
     RestaurantsModule,
   ],
