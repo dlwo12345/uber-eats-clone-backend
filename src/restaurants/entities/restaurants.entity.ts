559e70bb9122c9f65d5b4f3190be6540b665e55f
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-04 11:39:47 +0900

    restuarant 엔티티 필수값 설정 변경

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @InputType({ isAbstract: true })
@@ -16,8 +16,12 @@ export class Restaurant {
   @Length(5, 10)
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, {
+    defaultValue: true, // graphql 스키마 상의 defaultvalue = true
+    // nullable: true, // 필수값 해제
+  })
+  @Column({ default: true }) // database default value = true
+  @IsOptional() // 필수값 제외
   @IsBoolean()
   isVegan: boolean;
 
