5c87cee7f5540c1adf3044ad56dc3bc28f5e5570
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-04 11:40:05 +0900

    updaterESTAURANT 형태 정의

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -22,5 +23,12 @@ export class RestaurantsResolver {
     } catch (e) {
       return false;
     }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args() updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    return true;
   }
 }
