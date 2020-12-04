36e1c9ab8994611c1fff9e94f150df5d48e5bdca
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-04 11:11:46 +0900

    validation entity로 이동

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,4 +1,5 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @InputType({ isAbstract: true })
@@ -10,22 +11,28 @@ export class Restaurant {
   id: number;
 
   @Field((type) => String)
+  @IsString()
   @Column()
+  @Length(5, 10)
   name: string;
 
   @Field((type) => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field((type) => String)
+  @IsString()
   @Column()
   ownerName: string;
 
   @Field((type) => String)
+  @IsString()
   @Column()
   categoryName: string;
 }
