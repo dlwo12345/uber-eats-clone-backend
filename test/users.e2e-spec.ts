e502034d5b6381a3163b14c64d6d120091e87aa9
jaehong.lee <jaehong.lee@solarconnect.kr>
2021-01-12 10:32:30 +0900

    createAccount, login e2e 테스트코드 작성

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,10 +4,21 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const testUser = {
+  email: 'test@dumy.co.kr',
+  password: '12345',
+};
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,7 +35,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'nico@las.com';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -32,8 +42,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email:"${EMAIL}",
-              password:"12345",
+              email:"${testUser.email}",
+              password:"${testUser.password}",
               role:Owner
             }) {
               ok
@@ -43,16 +53,100 @@ describe('UserModule (e2e)', () => {
           `,
         })
         .expect(200)
-        .expect(res=>{
+        .expect((res) => {
           expect(res.body.data.createAccount.ok).toBe(true);
           expect(res.body.data.createAccount.error).toBe(null);
         });
     });
+
+    it('계정이 있을때 실패해야합니다.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email:"${testUser.email}",
+            password:"${testUser.password}",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(
+            '동일한 email을 가진 유저가 존재합니다.',
+          );
+        });
+    });
   });
 
-  it.todo('me');
+  describe('login', () => {
+    it('올바른 login 절차에 따라 로그인해야합니다.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+          login(input:{
+            email:"${testUser.email}",
+            password:"${testUser.password}",
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+    it('잘못된 login 절차로 로그인을 할 수 없어야합니다.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+          login(input:{
+            email:"${testUser.email}",
+            password:"xxxxx",
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('패스워드가 다릅니다');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
   it.todo('userProfile');
-  it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
